af91c43a63ed8038e742b5eeedd9613e068aaf1d
Brandon Ngo <brandonngo56@gmail.com>
2025-03-20 19:54:50 -0400

    Add favicon

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
     res.render("index");
 });
